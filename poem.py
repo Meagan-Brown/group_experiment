4b6f940ba73569d88ec76951b75120fd24b77d67
Phillip <194154132+pdschneider@users.noreply.github.com>
2025-11-06 21:48:30 +0000

    my poem part

--- poem.py
+++ poem.py
@@ -2,7 +2,7 @@ poem = "\nRoses are red, \n"
 poem += "Violets are blue, \n"
 
 # Add a line to this, be respectful, avoid unappropriate statements
-
+poem += "My toaster ghosted me, now it only burns 'U up?' into my bread.\n"
 # poem += "Add your phrase\n"
 
 print(poem)
